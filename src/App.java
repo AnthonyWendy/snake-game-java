d7d826988e907571bb682a8ba98f31a3af34a983
AnthonyWendy <anthonywendyantunesdelima@gmail.com>
2024-07-16 21:18:43 -0300

    window_class

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        int boardWidth = 600;
+        int boardHeight = 600;
+
+        JFrame frame = new JFrame("Snake");
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
