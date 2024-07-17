cf10b7e66f3e0d318a8d1ac72dfbe3d6b5c5015f
AnthonyWendy <anthonywendyantunesdelima@gmail.com>
2024-07-16 21:43:50 -0300

    creation of the board

--- src/App.java
+++ src/App.java
@@ -3,13 +3,20 @@ import javax.swing.*;
 public class App {
     public static void main(String[] args) {
         int boardWidth = 600;
-        int boardHeight = 600;
+        int boardHeight = boardWidth;
 
         JFrame frame = new JFrame("Snake");
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // criacao do tabuleiro
+        SnakeGame snakeGame = new SnakeGame(boardWidth, boardHeight);
+
+        // inserindo o tabuleiro na janela
+        frame.add(snakeGame);
+        frame.pack();
     }
 }
---
+++ src/SnakeGame.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SnakeGame extends JPanel {
+    private class Tile{
+        int x;
+        int y;
+
+        Tile(int x, int y){
+            this.x = x;
+            this.y = x;
+        }
+    }
+
+
+    int boardWidth;
+    int boardHeight;
+    int tileSize = 25;
+
+    SnakeGame(int boardWidth, int boardHeight) {
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+        setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
+        setBackground(Color.BLACK);
+    }
+}
