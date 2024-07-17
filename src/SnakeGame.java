3f13feb6cdc626a86305ad681afc0d68b3277929
AnthonyWendy <anthonywendyantunesdelima@gmail.com>
2024-07-16 22:18:00 -0300

    created function for the random position on the fruit

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class SnakeGame extends JPanel {
     // posição dos blocos
@@ -22,6 +23,10 @@ public class SnakeGame extends JPanel {
 
     Tile snakeHead;
 
+    Tile food;
+
+    Random random;
+
     SnakeGame(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
@@ -30,6 +35,11 @@ public class SnakeGame extends JPanel {
 
         // Posição inicial da cabeça da cobra
         snakeHead = new Tile(5, 5);
+
+        // Posição inicial da fruta
+        food = new Tile(10, 10);
+        random = new Random();
+        placeFood();
     }
 
     // Função para pintar na tabuleiro
@@ -49,8 +59,19 @@ public class SnakeGame extends JPanel {
             g.drawLine(0, i*tileSize, boardWidth, i*tileSize); // Crua a linha Horizontal
         }
 
+        // Fruit
+        g.setColor(Color.RED);
+        g.fillRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize);
+
+
         //Snake
         g.setColor(Color.GREEN);
         g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize);
+    }
+
+    // Responsável por criar a posição aleatória da fruta
+    public void placeFood() {
+        food.x = random.nextInt(boardWidth/tileSize);
+        food.y = random.nextInt(boardHeight/tileSize);
     }
 }
